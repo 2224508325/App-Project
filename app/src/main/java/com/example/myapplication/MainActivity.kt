cb27ff72f7c875b41f8b1d05862dd683f48146ca
Joshua <qq1144102465@gmail.com>
2020-12-27 17:46:30 -0500

    做出来了我的activity//接下来尝试从firebase拿信息

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -25,40 +25,22 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AndyActivity::class.java)
             startActivity(intent)
         }
-        // for EricActivity
-        val eric: Button = findViewById(R.id.ericActivity)
-        eric.setOnClickListener {
-            startActivity(Intent(this,EricActivity::class.java))
+
+        val josh1: Button = findViewById(R.id.Joshua)
+        josh1.setOnClickListener {
+            val intent = Intent(this, JoshuaActivity::class.java)
+            startActivity(intent)
         }
-        Toast.makeText(this, "Firebase Connection Success", Toast.LENGTH_LONG).show()
 
-
-    }
-
-        fun setstop() {
-            findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show()
+            // for EricActivity
+            val eric: Button = findViewById(R.id.ericActivity)
+            eric.setOnClickListener {
+                startActivity(Intent(this, EricActivity::class.java))
             }
+            Toast.makeText(this, "Firebase Connection Success", Toast.LENGTH_LONG).show()
 
 
         }
 
-        override fun onCreateOptionsMenu(menu: Menu): Boolean {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            menuInflater.inflate(R.menu.menu_main, menu)
 
-            return true
-        }
-
-
-        override fun onOptionsItemSelected(item: MenuItem): Boolean {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            return when (item.itemId) {
-                R.id.action_settings -> true
-                else -> super.onOptionsItemSelected(item)
-            }
-        }
     }
