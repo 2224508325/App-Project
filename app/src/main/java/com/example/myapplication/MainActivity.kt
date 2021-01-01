b71441b4add014ff7907387e45ea4f98e6d4d9d8
quxy <hiandy321@gmail.com>
2020-12-31 21:16:52 -0500

    Bottom Three Button finished

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,9 +11,14 @@ import android.view.MenuItem
 
 
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.myapplication.fragments.DoctorFragment
+import com.example.myapplication.fragments.ProductFragmet
+import com.example.myapplication.fragments.SymptomFragment
 import com.google.zxing.integration.android.IntentIntegrator
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +34,32 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Firebase Connection Success", Toast.LENGTH_LONG).show()
 
 
+
+        //Andy's Part
+        val productFragment = ProductFragmet()
+        val symptomFragment = SymptomFragment()
+        val doctorFragment = DoctorFragment()
+        makeCurrentFragment(productFragment)
+        bottom_navigation.setOnNavigationItemSelectedListener{
+            when (it.itemId){
+                R.id.ic_product -> makeCurrentFragment(productFragment)
+                R.id.ic_symptom -> makeCurrentFragment(symptomFragment)
+                R.id.ic_doctor -> makeCurrentFragment(doctorFragment)
+            }
+            true
+        } //End of Andy's Part
+
+
+
+    }
+
+    //Andy Part
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
         }
+    //End Andy part
 
 
     }
