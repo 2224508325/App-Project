e8b974d36c7cab141af7b614f8c243e10e156bfa
quxy <hiandy321@gmail.com>
2021-01-01 13:26:04 -0500

    SearchFunction Done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -26,18 +26,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-/*
-       //for search
-        adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,resources.getStringArray(R.array.countries_array))
-        lv_listview.adapter = adapter
-        lv_listview.onItemClickListener = AdapterView.OnItemClickListener {parent,view,position,id ->
-            Toast.makeText(applicationContext,parent?.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT ).show()
-        }
-        lv_listview.emptyView = tv_emptyTextView
-        //search ended*/
-
-
-
 
         //Bottom Nav
         val productFragment = ProductFragmet()
@@ -57,27 +45,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
-/*    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.nav_search,menu)
-        val search = menu?.findItem(R.id.nav_search)
-        val searchView = search?.actionView as SearchView
-        searchView.queryHint = "Search Here"
-
-    searchView.setOnQueryTextListener(object:SearchView.OnQueryTextListener{
-        override fun onQueryTextSubmit(query: String?): Boolean {
-            return false
-        }
-
-        override fun onQueryTextChange(newText: String?): Boolean {
-            adapter.filter.filter(newText)
-            return true
-        }
-    })
-        return super.onCreateOptionsMenu(menu)
-
-    }
-    //end Top search*/
 
    //Bottom Nav
     private fun makeCurrentFragment(fragment: Fragment) =
