1d4c365f8d226fdcac42fd68063138342cdccde1
Joshua <qq1144102465@gmail.com>
2021-01-03 01:57:54 -0500

    symptom first page done

--- app/src/main/java/com/example/myapplication/fragments/SymptomFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/SymptomFragment.kt
@@ -1,20 +1,26 @@
 package com.example.myapplication.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.myapplication.R
+import com.example.myapplication.SearchActivity
+import com.example.myapplication.retrieveActivity2
 
 class SymptomFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_symptom, container, false)
-    }
 
+    }
 }
